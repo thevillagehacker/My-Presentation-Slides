b9b1b65d58f35b4ec63c7bc9b4fc36a57e44c3bd
Naveen J <nvnj1998@gmail.com>
2021-03-19 12:04:47 +0530

    Update BasicSerialize.java

--- Insecure_Deserialization/Examples/java/BasicSerialize.java
+++ Insecure_Deserialization/Examples/java/BasicSerialize.java
@@ -1,11 +1,10 @@
-
 import java.io.*;
 
 public class BasicSerialize {
   public static void main(String [] args) {
     try {
-      Human being = new Human(23, "naveenj", "thevillagehacker", 68);
-      String fname = "being.ser";
+      Human being = new Human(23, "naveenj", "thevillagehacker");
+      String fname = "humandata.ser";
       System.out.println("\nObject is");
       System.out.println(being);
       
@@ -34,3 +33,41 @@ public class BasicSerialize {
     }
   }
 }
+
+
+import java.io.*;
+
+public class BasicSerialize {
+  public static void main(String [] args) {
+    try {
+      Human being = new Human(23, "naveenj", "thevillagehacker");
+      String fname = "humandata.ser";
+      System.out.println("\nObject is");
+      System.out.println(being);
+      
+      // serializing an object
+      FileOutputStream fout = new FileOutputStream(fname);
+      ObjectOutputStream oout = new ObjectOutputStream(fout);
+      System.out.println("\nSerialization Happening here ...");
+      oout.writeObject(being);   // actual serialization
+      oout.close();
+      fout.close();
+      System.out.println("\nThe object was written to " + fname);
+
+      // deserializing an object
+      FileInputStream fin = new FileInputStream(fname);
+      ObjectInputStream oin = new ObjectInputStream(fin);
+      System.out.println("\nDeserialization Happening here ...");
+      Human Human = (Human) oin.readObject();   // actual deserialization
+      oin.close();
+      fin.close();      
+      System.out.println("\nThe object was read from " + fname + ":");
+      System.out.println(Human);
+      System.out.println();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
